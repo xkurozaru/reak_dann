74e198b5b7d1a5f045409abc6bb464df087938ad
xkurozaru <kurozaru.210@gmail.com>
2024-04-17 15:25:23 +0900

    add func

---
+++ models/function.py
@@ -0,0 +1,23 @@
+from torch.autograd import Function
+
+
+class GradientReversalLayer(Function):
+    @staticmethod
+    def forward(context, x, constant):
+        context.constant = constant
+        return x.view_as(x) * constant
+
+    @staticmethod
+    def backward(context, grad):
+        return grad.neg() * context.constant, None
+
+
+class GradientThroughLayer(Function):
+    @staticmethod
+    def forward(context, x, constant):
+        context.constant = constant
+        return x.view_as(x) * constant
+
+    @staticmethod
+    def backward(context, grad):
+        return grad * context.constant, None
